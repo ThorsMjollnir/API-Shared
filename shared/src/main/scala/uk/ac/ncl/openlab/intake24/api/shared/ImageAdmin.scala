83f4bf5d6baaf573bc1acdb74edad71ec78bb3e0
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2018-08-06 12:03:59 +0100

    new types

--- shared/src/main/scala/uk/ac/ncl/openlab/intake24/api/shared/ImageAdmin.scala
+++ shared/src/main/scala/uk/ac/ncl/openlab/intake24/api/shared/ImageAdmin.scala
@@ -1,6 +1,10 @@
 package uk.ac.ncl.openlab.intake24.api.data
 
 
-case class NewImageMapRequest(id: String, description: String, objectDescriptions: Map[String, String])
+case class NewImageMapRequest(id: String, description: String)
 
 case class NewGuideImageRequest(id: String, description: String, imageMapId: String, objectWeights: Map[String, Double])
+
+case class NewImageMapWithObjectsRequest(id: String, description: String, objectDescriptions: Map[String, String])
+
+case class ImageMapResponse(id: String, description: String, path: String)
